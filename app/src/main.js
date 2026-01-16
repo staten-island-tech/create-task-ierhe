d49341dbc019e246dc96226c9647ee434c4a2ffd
ierhe <yuningw05@gmail.com>
2026-01-16 12:20:42 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,16 @@
 import './style.css'
 
+
+
 const HIT = document.getElementById("HIT");
 const STAND = document.getElementById("STAND");
 
+const playertotal = [0]
+const AItotal = [0]
 
-
-
+const cardlist = [
+  {
+  Number: "",
+  },
+]
 
