dbd10dcf29f2a30ba26d91eca4c8730e67693e67
ierhe <yuningw05@gmail.com>
2026-01-27 11:58:17 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,18 +1,46 @@
 import './style.css'
 
-
+const cardlist = [
+  {
+  Number: "1",
+  },
+  {
+  Number: "2",
+  },
+  {
+  Number: "3",
+  },
+  {
+  Number: "4",
+  },
+  {
+  Number: "5",
+  },
+  {
+  Number: "6",
+  },
+  {
+  Number: "7",
+  },
+  {
+  Number: "8",
+  },
+  {
+  Number: "9",
+  },
+  {
+  Number: "10",
+  },
+]
 
 const HIT = document.getElementById("HIT");
 const STAND = document.getElementById("STAND");
+const HISTORY = document.getElementById("HISTORY")
 
 const playertotal = [0]
+
 const AItotal = [0]
 
 const wincounter = [0]
 
-const cardlist = [
-  {
-  Number: "",
-  },
-]
 
