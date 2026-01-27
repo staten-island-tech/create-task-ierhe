20a5a2148f13b215057f14d9e15f43994e202aa0
ierhe <yuningw05@gmail.com>
2026-01-27 12:29:13 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -2,34 +2,34 @@ import './style.css'
 
 const cardlist = [
   {
-  Number: "1",
+  Number: 1,
   },
   {
-  Number: "2",
+  Number: 2,
   },
   {
-  Number: "3",
+  Number: 3,
   },
   {
-  Number: "4",
+  Number: 4,
   },
   {
-  Number: "5",
+  Number: 5,
   },
   {
-  Number: "6",
+  Number: 6,
   },
   {
-  Number: "7",
+  Number: 7,
   },
   {
-  Number: "8",
+  Number: 8,
   },
   {
-  Number: "9",
+  Number: 9,
   },
   {
-  Number: "10",
+  Number: 10,
   },
 ]
 
@@ -48,10 +48,10 @@ const playercardtotal = [0];
 
 const algurithem = (playercardtotal, aicardtotal) => {
   while (playercardtotal < 21){
-    if (){
+    if (HIT = true){
       //player clicks hit button
     }
-    if (){
+    if (STAND = true){
       //player clicks stand button
     }
   } 
@@ -61,7 +61,8 @@ HIT.addEventListener("click", async function () {
   if (playercardtotal > 21) {
     console.log("You already LOST, start a new game")
   } else {
-
+    const rngnumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    generatednumber = cardlist.rngnumber
   }
 
 });
