9c35245095d9e6d67f68dd40393b56f35983f03e
ierhe <yuningw05@gmail.com>
2026-01-27 12:04:38 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -36,6 +36,7 @@ const cardlist = [
 const HIT = document.getElementById("HIT");
 const STAND = document.getElementById("STAND");
 const HISTORY = document.getElementById("HISTORY")
+const DELETE_HISTORY = document.getElementById("DELETE_HISTORY")
 
 const playertotal = [0]
 
@@ -43,4 +44,22 @@ const AItotal = [0]
 
 const wincounter = [0]
 
+const playercardtotal = [0]
 
+HIT.addEventListener("click", async function () {
+  if (playercardtotal = 0) {
+
+  }
+});
+
+STAND.addEventListener("click", async function () {
+
+});
+
+HISTORY.addEventListener("click", async function () {
+
+});
+
+DELETE_HISTORY.addEventListener("click", async function () {
+
+});
