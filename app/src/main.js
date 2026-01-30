57804518ebe6d56f52114b9bed537154d9d0a7c7
ierhe <yuningw05@gmail.com>
2026-01-30 12:10:59 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,55 +1,60 @@
 import './style.css'
 
-const ROCK = document.getElementById("ROCK");
-const PAPER = document.getElementById("PAPER");
-const SCISSOR = document.getElementById("SCISSOR")
-const START = document.getElementById("START")
-const playerwin = [0]
-let start = "false"
-let win = "false"
+const RROCK = document.getElementById("ROCK");
+const PPAPER = document.getElementById("PAPER");
+const SSCISSOR = document.getElementById("SCISSOR")
+const SSTART = document.getElementById("START")
+const playerwin = 0
+let start = false
+let win = false
 
-START.addEventListener("click", async function () {
-  start = "true"
+SSTART.addEventListener("click", async function () {
+  start = true
 });
 
+const stuff = ["paper, rock, scssior"]
 
-while ((start = "true")){
-  ROCK.addEventListener("click", async function () {
+async function something() {
+  await userinput
+}
+
+while (start && !win){
+  RROCK.addEventListener("click", async function () {
     const AIside = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    if (AIside = 3) {
+    if (AIside === 3) {
       console.log("Its a draw, none wins") //rock
-    } else if (AIside = 2) {
+    } else if (AIside === 2) {
       console.log("Its a win, nice") //scissors
       playerwin++
-      win = "true"
+      win = true
       return(win)
-    } if (AIside = 1) {
+    } if (AIside === 1) {
       console.log("Its a lost, go again!") //paper
     }
     });
-  PAPER.addEventListener("click", async function () {
+  PPAPER.addEventListener("click", async function () {
     const AIside = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    if (AIside = 3) {
+    if (AIside === 3) {
       console.log("Its a win, nice") //rock
       playerwin++
-      win = "true"
+      win = true
       return(win)
-    } else if (AIside = 2) {
+    } else if (AIside === 2) {
       console.log("Its a lost, go again!") //scissors
-    } if (AIside = 1) {
+    } if (AIside === 1) {
       console.log("Its a draw, none wins") //paper
-}
+    }
     });
-  SCISSOR.addEventListener("click", async function () {
+  SSCISSOR.addEventListener("click", async function () {
     const AIside = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-    if (AIside = 3) {
+    if (AIside === 3) {
       console.log("Its a lost, go again!") //rock
-    } else if (AIside = 2) {
+    } else if (AIside === 2) {
       console.log("Its a draw, none wins") //scissors
-    } if (AIside = 1) {
+    } if (AIside === 1) {
       console.log("Its a win, nice") //paper
       playerwin++
-      win = "true"
+      win = true
       return(win)
     }
   });
