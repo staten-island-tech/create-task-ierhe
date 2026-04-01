353325a97f87da25155aa0805144fc4d38f07a38
ierhehe <yuningw05@gmail.com>
2026-03-31 18:20:57 -0700

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -4,14 +4,25 @@ const RROCK = document.getElementById("ROCK");
 const PPAPER = document.getElementById("PAPER");
 const SSCISSOR = document.getElementById("SCISSOR");
 const SSTART = document.getElementById("START");
-let win = false;
 let history = [];
 let result = "";
 const historybox = document.querySelector('.History');
+let round = 0;
 
 SSTART.addEventListener("click", function () {
-  game()
+  historybox.innerHTML = "";
+  historybox.innerHTML += `<div>Game Reset! Can play 5 rounds again</div>`;
+  RROCK.disabled = false;
+  PPAPER.disabled = false;
+  SSCISSOR.disabled = false;
+  round = 0;
 });
+
+const winConditions = {
+  rock: "scissor",
+  paper: "rock",
+  scissor: "paper"
+};
 
 const stuff = ["paper", "rock", "scissor"]
 
@@ -19,39 +30,36 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function game(){
-  while (!win) {
-    RROCK.addEventListener("click", () => thegame("rock"));
-    PPAPER.addEventListener("click", () => thegame("paper"));
-    SSCISSOR.addEventListener("click", () => thegame("scissor"));
-    break;
-  }
-}
+RROCK.addEventListener("click", () => thegame("rock"));
+PPAPER.addEventListener("click", () => thegame("paper"));
+SSCISSOR.addEventListener("click", () => thegame("scissor"));
 
 function thegame(something){
-  const computer = getRandomInt(stuff.length);
-  const computerother = stuff[computer];
-  if (something === computerother) {
-    console.log("It's a draw");
-    history.push("draw!");
-    result = ("draw!");
-    historybox.innerHTML += `<div>${result}</div>`;
-  } 
-  else if (
-    (something === "rock" && computerother === "scissor") ||
-    (something === "paper" && computerother === "rock") ||
-    (something === "scissor" && computerother === "paper")
-  ) {
-    console.log("WIN!");
-    win = true;
-    result = ("WIN!");
-    historybox.innerHTML += `<div>${result}</div>`;
-  } 
-  else {
-    console.log("again");
-    history.push("LOSE");
-    result = ("LOST");
-    historybox.innerHTML += `<div>${result}</div>`;
+  while (round < 5) {
+    const computer = getRandomInt(stuff.length);
+    const computerother = stuff[computer];
+    if (something === computerother) {
+      console.log("It's a draw");
+      result = ("draw!");
+    } 
+    else if (winConditions[something] === computerother) {
+      console.log("WIN!");
+      result = "WIN!";
+    } 
+    else {
+      console.log("again");
+      result = "LOST";
+    }
+    history.push(result);
+    historybox.innerHTML += `<div>Round ${round + 1}: ${result}</div>`;
+    round++;
+    break;
   }
-  
+  if (round >= 5) {
+    console.log("Maximum rounds reached!");
+    historybox.innerHTML += `<div>Game Over! Max 5 rounds reached.</div>`;
+    RROCK.disabled = true;
+    PPAPER.disabled = true;
+    SSCISSOR.disabled = true;
+  }
 }
